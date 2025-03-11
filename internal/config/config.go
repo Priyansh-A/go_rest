389eda128e58a5e81faa362a82151978d3e3f322
Priyanshu Awasthi <nppriyansh112@gmail.com>
2025-03-11 10:00:16 +0545

    configadd

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,4 +1,41 @@
 package config
 
-type config struct {
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+type HTTPServer struct {
+	Addr string
+}
+
+// env-default: production . when we dont pass any environment
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+func MustLoad() {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the configuration path")
+		flag.Parse()
+		configPath = *flags
+		if configPath == "" {
+			log.Fatal("config path is not set")
+		}
+
+	}
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist : %s, configPath")
+	}
+	var cfg struct 
+
+	
 }
